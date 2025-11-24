0b2d8ecd819bfdb034fbb7af306d83a7b46b4f4b
skaur1604 <skaur1604@academic.rrc.ca>
2025-11-24 13:27:19 -0600

    update recipe controller.ts file

--- src/controllers/recipeController.ts
+++ src/controllers/recipeController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { db } from "../config/firebase";
 
 let recipes: any[] = [];
 
@@ -28,4 +29,51 @@ res.status(200).json(recipes[index]);
 export const deleteRecipe = (req: Request, res: Response) => {
 recipes = recipes.filter(r => r.id !== req.params.id);
 res.status(200).json({ message: "Recipe deleted" });
+};
+
+export const filterRecipes = async (req: any, res: any) => {
+  try {
+    const { category, minRating, search, sortBy, order, page, limit } = req.query;
+
+    let queryRef: FirebaseFirestore.Query = db.collection("recipes");
+
+    if (category) {
+      queryRef = queryRef.where("category", "==", category);
+    }
+
+    if (minRating) {
+      queryRef = queryRef.where("rating", ">=", Number(minRating));
+    }
+    if (search) {
+      const keyword = search.toLowerCase();
+      queryRef = queryRef.where("keywords", "array-contains", keyword);
+    }
+    if (sortBy) {
+      queryRef = queryRef.orderBy(sortBy, order === "desc" ? "desc" : "asc");
+    }
+
+    const snapshot = await queryRef.get();
+    const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    const pageNum = Number(page) || 1;
+    const limitNum = Number(limit) || 10;
+    const start = (pageNum - 1) * limitNum;
+    const paginated = data.slice(start, start + limitNum);
+
+    return res.status(200).json({
+      success: true,
+      total: data.length,
+      page: pageNum,
+      limit: limitNum,
+      results: paginated,
+    });
+
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error(err.message);
+      return res.status(500).json({ message: err.message });
+    }
+
+    console.error(err);
+    return res.status(500).json({ message: "Unknown error occurred" }); 
+  }
 };
