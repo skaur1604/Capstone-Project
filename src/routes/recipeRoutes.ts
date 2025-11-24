d4808568e8bbb6725b63f748b53f2ae0b88844b3
skaur1604 <skaur1604@academic.rrc.ca>
2025-11-24 13:32:32 -0600

    update recipe routes.ts file

--- src/routes/recipeRoutes.ts
+++ src/routes/recipeRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { getAllRecipes, getRecipeById, createRecipe, updateRecipe, deleteRecipe } from "../controllers/recipeController";
 import upload from "../middleware/upload";
+import { filterRecipes } from "../controllers/recipeController";
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ router.get("/:id", getRecipeById);
 router.post("/", createRecipe);
 router.put("/:id", updateRecipe);
 router.delete("/:id", deleteRecipe);
+router.get("/filter", filterRecipes);
 
 router.post("/:id/upload", upload.single("image"), (req, res) => {
 res.status(200).json({ message: "Image uploaded successfully", file: req.file });
